848ec320bc77a10fa7a3e244950723847298c8c9
anshumansingh-06 <git@stackblitz.com>
2024-10-19 14:32:51 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const { resolve } = require('path');
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+
+app.use(express.static('static'));
+app.use(cors());
+
+app.get('/cart-total', (req, res) => {
+  let newItemPrice = parseFloat(app.query.newItemPrice);
+  let cartTotal = parseFloat(app.query.cartTotal);
+  let result = newItemPrice + cartTotal;
+  cartTotal = result;
+  res.send(result.toString());
+});
+app.get('/membership-discount', (req, res) => {
+  let cartTotal = parseFloat(app.query.cartTotal);
+  let isMember = app.query.isMember === 'true';
+  let result;
+  if (isMember) {
+    result = cartTotal - (cartTotal * discountPrice) / 100;
+    cartTotal = result;
+  } else {
+    result = cartTotal;
+  }
+  res.send(result.toString());
+});
+app.get('/calculate-tax', (req, res) => {
+  let cartTotal = parseFloat(app.query.cartTotal);
+  let result = cartTotal + (cartTotal * tax) / 100;
+  res.send(result.toString());
+});
+app.get('/estimate-delivery', (req, res) => {
+  let distance = parseFloat(app.query.distance);
+  let shippingMethod = app.query.shippingMethod;
+  let result;
+  if (shippingMethod === Standard) {
+    result = distance / 100;
+  } else {
+    result = distance / 50;
+  }
+  res.send(result.toString());
+});
+app.get('/shipping-cost', (req, res) => {
+  let weight = parseFloat(app.query.weight);
+  let distance = parseFloat(app.query.distance);
+  let result = weight * distance * 0.1;
+  res.send(result.toString());
+});
+app.get('/loyalty-points', (req, res) => {
+  let purchaseAmount = parseFloat(app.query.purchaseAmount);
+  let result = purchaseAmount * 2;
+  res.send(result.toString());
+});
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
