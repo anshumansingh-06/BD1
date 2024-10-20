0ead118965c7321086863c59b12dc510267d4956
anshumansingh-06 <139244709+anshumansingh-06@users.noreply.github.com>
2024-10-20 14:04:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,15 +9,15 @@ app.use(express.static('static'));
 app.use(cors());
 
 app.get('/cart-total', (req, res) => {
-  let newItemPrice = parseFloat(app.query.newItemPrice);
-  let cartTotal = parseFloat(app.query.cartTotal);
+  let newItemPrice = parseFloat(req.query.newItemPrice);
+  let cartTotal = parseFloat(req.query.cartTotal);
   let result = newItemPrice + cartTotal;
   cartTotal = result;
   res.send(result.toString());
 });
 app.get('/membership-discount', (req, res) => {
-  let cartTotal = parseFloat(app.query.cartTotal);
-  let isMember = app.query.isMember === 'true';
+  let cartTotal = parseFloat(req.query.cartTotal);
+  let isMember = req.query.isMember === 'true';
   let result;
   if (isMember) {
     result = cartTotal - (cartTotal * discountPrice) / 100;
@@ -28,13 +28,13 @@ app.get('/membership-discount', (req, res) => {
   res.send(result.toString());
 });
 app.get('/calculate-tax', (req, res) => {
-  let cartTotal = parseFloat(app.query.cartTotal);
+  let cartTotal = parseFloat(req.query.cartTotal);
   let result = cartTotal + (cartTotal * tax) / 100;
   res.send(result.toString());
 });
 app.get('/estimate-delivery', (req, res) => {
-  let distance = parseFloat(app.query.distance);
-  let shippingMethod = app.query.shippingMethod;
+  let distance = parseFloat(req.query.distance);
+  let shippingMethod = req.query.shippingMethod;
   let result;
   if (shippingMethod === Standard) {
     result = distance / 100;
@@ -44,13 +44,13 @@ app.get('/estimate-delivery', (req, res) => {
   res.send(result.toString());
 });
 app.get('/shipping-cost', (req, res) => {
-  let weight = parseFloat(app.query.weight);
-  let distance = parseFloat(app.query.distance);
+  let weight = parseFloat(req.query.weight);
+  let distance = parseFloat(req.query.distance);
   let result = weight * distance * 0.1;
   res.send(result.toString());
 });
 app.get('/loyalty-points', (req, res) => {
-  let purchaseAmount = parseFloat(app.query.purchaseAmount);
+  let purchaseAmount = parseFloat(req.query.purchaseAmount);
   let result = purchaseAmount * 2;
   res.send(result.toString());
 });
